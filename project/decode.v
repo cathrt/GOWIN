9d63435ba967f560a490336ccee8e4d9f3670ba7
cathtr <3554180564@qq.com>
2026-09-18 00:34:53 +0800

    新加了清零信号

--- project/decode.v
+++ project/decode.v
@@ -1,14 +1,17 @@
 //编码器解码模块
-module decode(
+module decode #(
+	parameter WIDTH_DATA = 32
+) (
       input  wire clk,
       input  wire rst_n,
+	  // 控制端
+	  input  wire pos_clr,    //在从起摆转向平衡时清零计数
       //输入
       input  wire encode_a,
       input  wire encode_b,
       //输出
       output wire motor_dir,
-      output wire signed [31:0] cur_pos,
-      output wire signed [15:0] cur_speed
+      output wire signed [WIDTH_DATA-1:0] cur_pos
   );
 
   wire encode_pluse; //边沿脉冲，电机转一圈产生13*20*4=1040个脉冲
@@ -22,21 +25,18 @@ module decode(
       .encode_pluse(encode_pluse),
       .motor_dir(motor_dir)
     );
+
   //位置计数器模块
-  decode_cnt  decode_cnt_inst (
-      .clk(clk),
-      .rst_n(rst_n),
-      .encode_pluse(encode_pluse),
-      .motor_dir(motor_dir),
-      .cur_pos(cur_pos)
-    );
-  //角速度计算模块
-  decode_speed decode_speed_inst (
-      .clk(clk),
-      .rst_n(rst_n),
-      .encode_pluse(encode_pluse),
-      .motor_dir(motor_dir),
-      .cur_speed(cur_speed)
-    );
+  decode_cnt # (
+    .WIDTH_DATA(WIDTH_DATA)
+  )
+  decode_cnt_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .pos_clr(pos_clr),
+    .encode_pluse(encode_pluse),
+    .motor_dir(motor_dir),
+    .cur_pos(cur_pos)
+  );
 
   endmodule
--- project/decode_cnt.v
+++ project/decode_cnt.v
@@ -3,20 +3,24 @@
 将电机转动的角度数字化（位置），即用 1 来代表 360/1040 度
 正转+1，反转-1
 */
-module decode_cnt(
-    input wire clk,
-    input wire rst_n,
+module decode_cnt #(
+    parameter WIDTH_DATA = 32
+)(
+    input  wire clk,
+    input  wire rst_n,
+    // 控制端
+    input  wire pos_clr,    //在从起摆转向平衡时清零计数
     //输入
-    input wire encode_pluse,            //边沿脉冲
-    input wire motor_dir,               //电机的转向，[1]为正转，[0]为反转
+    input  wire encode_pluse,            //边沿脉冲
+    input  wire motor_dir,               //电机的转向，[1]为正转，[0]为反转
     //输出
-    output reg signed [31:0] cur_pos    //当前电机转动信息，1 代表 360/1040 度
+    output reg signed [WIDTH_DATA-1:0] cur_pos    //当前电机转动信息，1 代表 360/1040 度
 
 );
 
 always @(posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-      cur_pos <= 32'sd0;
+    if(!rst_n || pos_clr) begin
+      cur_pos <= 'sd0;
     end
     else if(encode_pluse) begin
         if(motor_dir) begin
